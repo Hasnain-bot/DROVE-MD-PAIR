15cfb98877549e3adf1498c1003e86a3c3f92a59
Hasnain-MD <mhasnainusman728@gmail.com>
2025-01-30 21:18:01 +0500

    Update app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -93,7 +93,7 @@ const App = () => {
     <div className="container">
       <form onSubmit={submit}>
         <BsPerson className="icon" />
-        <h1>PANHWAR-MD-PAIR</h1>
+        <h1>HASNAIN-MD-PAIR</h1>
         {res.status && (
           <Alert variant={res.is}>
             {res.msg}
